87f93b210b847569267f40d2fb55eeb9333c9711
kamalnathvg <kamalnath7171@gmail.com>
2026-09-26 10:38:31 -0400

    updated santhigiri events translation

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.a2g9urnsnis"
+    "revision": "0.4qecdghb6e8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
